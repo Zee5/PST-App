5dc4af433f22eddaf004989b80211248d695c7c6
Zelalem Tolera <tolerazelalem@gmail.com>
2018-02-22 03:16:05 -0500

    addloockus for stock detail and create view

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,7 @@
+class StocksController < ApplicationController
+    def search
+        @stock = Stock.new_from_lookup(params[:stock])
+        render 'users/my_portfolio'
+    end
+    
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,11 @@
-class Stock < ApplicationRecord
+class Stock < ActiveRecord::Base
+    def self.new_from_lookup(ticker_symbol)
+        looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+        price = strip_commas(looked_up_stock.l) # strip the comma price 
+        new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+    end
+    #strip cooma method to remove commas
+    def self.strip_commas(number)
+        number.gsub(",", "")   
+    end
 end
