ff96f9c040a66117a4a6599f46d65e70552e8905
Zelalem Tolera <tolerazelalem@gmail.com>
2018-02-22 23:04:02 -0500

    stock table checked after issue

--- db/migrate/20180223012514_create_stocks.rb
+++ db/migrate/20180223012514_create_stocks.rb
@@ -1,9 +1,11 @@
 class CreateStocks < ActiveRecord::Migration[5.1]
   def change
+    # created  stock table after psql issue
     create_table :stocks do |t|
       t.string :ticker
       t.string :name
       t.decimal :last_price
+
       t.timestamps
     end
   end
